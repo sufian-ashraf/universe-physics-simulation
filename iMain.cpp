0f4324a5bb718e28ea3a3fd2009fbcf4b97c0974
Shafin <shafinashraf074@gmail.com>
2024-02-27 13:58:34 +0600

    small fixes

--- iMain.cpp
+++ iMain.cpp
@@ -174,7 +174,7 @@ int main()
 {
 	// place your own initialization codes here.
 	// iSetTimer(1000, iDraw);
-	is_symmetric = true;
+	collsion_on = true;
 	running = true;
 
 	default_btn->position.y -= default_btn->dimensions.y;
